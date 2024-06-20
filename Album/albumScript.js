65ab3a10ac3e48a18af247166df9d61bf8dd4ea5
patriziapatti <patrizia.patti5@gmail.com>
2024-06-20 18:20:15 +0100

    fifth commit

--- Album/albumScript.js
+++ Album/albumScript.js
@@ -3,6 +3,8 @@ let id = ""
 let image = document.getElementById("fotoA")
 let imageArtist = document.getElementById("fotoArtista")
 let tableBody = document.getElementById("tabellaCanzoni")
+let artist = document.getElementById("artistaAlbum")
+let albumName = document.getElementById("albumName")
 
 document.addEventListener("DOMContentLoaded", async () => {
     //console.log("pagina caricata")
@@ -13,6 +15,13 @@ document.addEventListener("DOMContentLoaded", async () => {
     // getAlbumData2(id)
 })
 
+function convertiDurata(durata){
+    let minuti = Math.floor(durata/60)
+    let secondiRimanenti = durata % 60
+
+    return { minuti, secondiRimanenti }
+}
+
 let getAlbumData = function(id){
     fetch (url + id)
     .then((response)=>{
@@ -21,26 +30,34 @@ let getAlbumData = function(id){
         console.log(data)
         image.src = data.cover_medium
         imageArtist.src = data.artist.picture_small
+        console.log(data.duration)
+        let durataAlbum = convertiDurata(data.duration)
+        let formattaDurataAlbum = `${durataAlbum.minuti} min ${durataAlbum.secondiRimanenti} sec`
+        artist.innerHTML = data.artist.name + " · 2017 · " + formattaDurataAlbum + " · " + data.nb_tracks + " brani"
+        albumName.innerHTML = data.title
         data.tracks.data.forEach((element, i) => {
             //console.log(element)
-        // tableBody.innerHTML += `<tr>
-        //             <th scope="row" class="d-md-table-cell d-none pt-3">${i+1}</th>
-        //             <td><div>${element.title}</div><div class="text-white" style="--bs-text-opacity: .6;">${element.artist.name}</div></td>
-        //             <td class="d-md-table-cell d-none pt-3 ">${element.rank}</td>
-        //             <td class="d-md-table-cell d-none pt-3">${element.duration}</td>
-        //           </tr>`
-            let riga = document.createElement("tr")
-            riga.innerHTML =`<tr>
+        let durataTraccia = convertiDurata(element.duration)
+        let formattaDurataTraccia = `${durataTraccia.minuti}:${durataTraccia.secondiRimanenti}`
+        tableBody.innerHTML += `<tr>
                     <th scope="row" class="d-md-table-cell d-none pt-3">${i+1}</th>
                     <td><div>${element.title}</div><div class="text-white" style="--bs-text-opacity: .6;">${element.artist.name}</div></td>
                     <td class="d-md-table-cell d-none pt-3 ">${element.rank}</td>
-                    <td class="d-md-table-cell d-none pt-3">${element.duration}</td>
+                    <td class="d-md-table-cell d-none pt-3">${formattaDurataTraccia}</td>
                   </tr>`
-            tableBody.appendChild(riga)
+            // let riga = document.createElement("tr")
+            // riga.innerHTML =`<tr>
+            //         <th scope="row" class="d-md-table-cell d-none pt-3">${i+1}</th>
+            //         <td><div>${element.title}</div><div class="text-white" style="--bs-text-opacity: .6;">${element.artist.name}</div></td>
+            //         <td class="d-md-table-cell d-none pt-3 ">${element.rank}</td>
+            //         <td class="d-md-table-cell d-none pt-3">${element.duration}</td>
+            //       </tr>`
+            // tableBody.appendChild(riga)
         });  
         })
     })
 }
+
 
 
 
